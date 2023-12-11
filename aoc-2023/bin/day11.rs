1888fc6938a21c5ad72669d16f666648246bd059
Nikita Akatyev <frfntdakatevin@gmail.com>
2023-12-11 11:40:44 +0100

    AoC 2023: Day 11

---
+++ aoc-2023/bin/day11.rs
@@ -0,0 +1,8 @@
+use aoc_2023::day11;
+use aoc_common::Solution;
+
+fn main() {
+    Solution::new("Day 11: DISPLAY NAME")
+        .solution(|iter| day11::get_answer(iter))
+        .run("inputs/day11.txt");
+}
---
+++ aoc-2023/src/day11/mod.rs
@@ -0,0 +1,41 @@
+use aoc_common::util::{self, MatrixIndex};
+
+pub fn get_answer(lines: impl Iterator<Item = String>) -> util::GenericResult<(usize, usize)> {
+    let mut cells = Vec::new();
+
+    for (row, line) in lines.enumerate() {
+        let cell_cols = line.chars().enumerate().filter_map(|(col, ch)| (ch == '#').then_some(col));
+
+        cells.extend(cell_cols.map(|col| MatrixIndex { row, col }));
+    }
+
+    cells.sort_unstable_by_key(|cell| cell.row);
+    let mut empty_rows = 0;
+    let mut prev = cells[0];
+    for cell in cells.iter_mut() {
+        empty_rows += (cell.row - prev.row).saturating_sub(1);
+        prev = *cell;
+        cell.row += empty_rows * 999999;
+    }
+
+    cells.sort_unstable_by_key(|cell| cell.col);
+    let mut empty_cols = 0;
+    let mut prev = cells[0];
+    for cell in cells.iter_mut() {
+        empty_cols += (cell.col - prev.col).saturating_sub(1);
+        prev = *cell;
+        cell.col += empty_cols * 999999;
+    }
+
+    let mut distance_sum = 0;
+    for cell in cells.iter() {
+        for other in cells.iter() {
+            if cell == other {
+                continue;
+            }
+            distance_sum += usize::abs_diff(cell.row, other.row) + usize::abs_diff(cell.col, other.col);
+        }
+    }
+
+    Ok((distance_sum / 2, 0))
+}
